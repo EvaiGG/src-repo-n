ab6785849659672ffcc7fc3ed7bada1351d915e5
tar-zxvf <1415945977@qq.com>
2024-10-02 20:17:25 +0800

    1

---
+++ src/com/tony/config/WebMvcConfig.java
@@ -0,0 +1,12 @@
+package src.com.tony.config;
+
+public class WebMvcConfig {
+    
+    private String class_path = null;
+    private String loc = null;
+    
+    public WebMvcConfig() {
+        this.class_path = "class_path";
+        this.loc = "loc";
+    }
+}

277b0757e25c8b25bcbefc85dfc7c95188c4254b
tar-zxvf <1415945977@qq.com>
2024-10-02 23:03:10 +0800

    wrewr

--- src/com/tony/config/SimpConfig.java
+++ src/com/tony/config/SimpConfig.java
@@ -4,5 +4,12 @@ public class SimpConfig {
     
     public SimpConfig(){
         System.out.println("SimpConfig");
+        System.out.println("sdfjoie");
+        System.out.println("dsfe");
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
